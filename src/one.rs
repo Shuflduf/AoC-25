1a32d2341717112ea55c1862015c6f0cf1af595d
Shuflduf <shuflduf@gmail.com>
2025-12-02 09:10:05 -0700

    trying zig

--- src/one.rs
+++ src/one.rs
@@ -1,42 +1,32 @@
 use std::fs;
 
+// attempts
+// 6165
+// 6707
+// 6664
+
 pub fn run() {
-    let mut times_at_zero = 0;
     let mut times_past_zero = 0;
     let mut position = 50;
 
     let file_contents = fs::read_to_string("inputs/one.txt").unwrap();
+    // let file_contents = "R1000";
     file_contents.lines().for_each(|s| {
         let mut chars = s.chars();
-        let direction = chars.next().unwrap();
+        let direction = if chars.next().unwrap() == 'R' { 1 } else { -1 };
         let amount = chars.collect::<String>().parse::<i32>().unwrap();
 
-        if direction == 'R' {
-            times_past_zero += handle_rotation(&mut position, amount);
-        } else {
-            times_past_zero += handle_rotation(&mut position, -amount);
-        }
-
-        if position == 0 {
-            times_at_zero += 1;
-            times_past_zero += 1;
+        for _ in 0..amount {
+            position += direction;
+            if position < 0 {
+                position += 100;
+                times_past_zero += 1
+            } else if position >= 100 {
+                position -= 100;
+                times_past_zero += 1
+            }
         }
     });
 
-    println!("Arrived at zero {times_at_zero} times");
     println!("Passed zero {times_past_zero} times");
-}
-
-fn handle_rotation(current_rotation: &mut i32, difference: i32) -> i32 {
-    let mut times_past_zero = 0;
-    *current_rotation += difference;
-    while *current_rotation >= 100 {
-        times_past_zero += 1;
-        *current_rotation -= 100;
-    }
-    while *current_rotation < 0 {
-        times_past_zero += 1;
-        *current_rotation += 100;
-    }
-    times_past_zero
 }
