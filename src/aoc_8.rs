a16a99a0b1a297c6d009ade6b7c29974c01a584b
Shuflduf <shuflduf@gmail.com>
2025-12-08 20:49:59 -0700

    DID DAY 8 BEFORE DAY 7

--- src/aoc_8.rs
+++ src/aoc_8.rs
@@ -1,82 +1,12 @@
 use std::collections::HashMap;
 
-// 1000 too low
-
-// const INPUT: &str = include_str!("../inputs/8.txt");
-const INPUT: &str = r"162,817,812
-57,618,57
-906,360,560
-592,479,940
-352,342,300
-466,668,158
-542,29,236
-431,825,988
-739,650,466
-52,470,668
-216,146,977
-819,987,18
-117,168,530
-805,96,715
-346,949,466
-970,615,88
-941,993,340
-862,61,35
-984,92,344
-425,690,689";
-const CONNECTION_COUNT: usize = 10;
-
-// https://en.wikipedia.org/wiki/Disjoint-set_data_structure
-struct Circuits {
-    roots: Vec<usize>,
-    sizes: Vec<usize>,
-}
-
-impl Circuits {
-    // start each box as its own curcuit
-    fn new(size: usize) -> Self {
-        Circuits {
-            roots: (0..size).collect(),
-            sizes: vec![1; size],
-        }
-    }
-
-    fn find_root(&mut self, box_id: usize) -> usize {
-        if self.roots[box_id] != box_id {
-            self.roots[box_id] = self.find_root(self.roots[box_id]);
-        }
-        self.roots[box_id]
-    }
-
-    fn connect(&mut self, box_a: usize, box_b: usize) -> bool {
-        let root_a = self.find_root(box_a);
-        let root_b = self.find_root(box_b);
-
-        if root_a == root_b {
-            return false;
-        }
-
-        // attach smaller to larger
-        if self.sizes[root_a] < self.sizes[root_b] {
-            self.roots[root_a] = root_b;
-            self.sizes[root_b] += self.sizes[root_a];
-        } else {
-            self.roots[root_b] = root_a;
-            self.sizes[root_a] += self.sizes[root_b];
-        }
-        true
-    }
-
-    fn get_sizes(&mut self) -> Vec<usize> {
-        let mut sizes = HashMap::new();
-        for i in 0..self.roots.len() {
-            let root = self.find_root(i);
-            *sizes.entry(root).or_insert(0) += 1;
-        }
-        sizes.values().copied().collect()
-    }
-}
+const INPUT: &str = include_str!("../inputs/8.txt");
+const CONNECTION_COUNT: usize = 1000;
 
 fn main() {
+    let mut part_one_solution = 0;
+    let mut part_two_solution = 0;
+
     let boxes: Vec<(i64, i64, i64)> = INPUT
         .lines()
         .map(|line| {
@@ -97,35 +27,61 @@ fn main() {
 
     edges.sort_unstable_by_key(|&(dist, _, _)| dist);
 
-    let mut circuits = Circuits::new(box_count);
+    let mut circuits: Vec<usize> = (0..box_count).collect();
     let mut connections_left = CONNECTION_COUNT;
 
     for (_, box_a, box_b) in edges {
-        if circuits.connect(box_a, box_b) {
-            println!("connected {:?} to {:?}", boxes[box_a], boxes[box_b]);
+        let root_a = find_root(&circuits, box_a);
+        let root_b = find_root(&circuits, box_b);
 
-            connections_left -= 1;
-            if connections_left <= 0 {
-                break;
+        if root_a != root_b {
+            for i in 0..circuits.len() {
+                if circuits[i] == root_b {
+                    circuits[i] = root_a;
+                }
             }
+        }
+
+        if connections_left > 0 {
+            connections_left -= 1;
+        }
+        if connections_left == 0 && part_one_solution == 0 {
+            let mut sizes = get_sizes(&circuits);
+            sizes.sort_unstable_by(|a, b| b.cmp(a));
+
+            part_one_solution = sizes.iter().take(3).product();
+        }
+        if all_connected(&circuits) {
+            part_two_solution = boxes[box_a].0 * boxes[box_b].0;
+            break;
         }
     }
 
-    let mut sizes = circuits.get_sizes();
-    sizes.sort_unstable_by(|a, b| b.cmp(a));
-    // sizes.sort_unstable();
-
-    println!("{sizes:?}");
-
-    // println!("Made {} successful connections", 1000 - connections_left);
-    // println!("Top 3 circuit sizes: {:? }", &sizes[..3]);
-
-    let part_one_solution: usize = sizes.iter().take(3).product();
-
-    // println!("{boxes:?}");
     println!("Part 1: {part_one_solution}");
+    println!("Part 2: {part_two_solution}");
 }
 
 fn distance_squared(first: &(i64, i64, i64), second: &(i64, i64, i64)) -> i64 {
     return (second.0 - first.0).pow(2) + (second.1 - first.1).pow(2) + (second.2 - first.2).pow(2);
+}
+
+fn get_sizes(circuits: &[usize]) -> Vec<u32> {
+    let mut sizes = HashMap::new();
+    for root_id in circuits.iter() {
+        *sizes.entry(root_id).or_insert(0) += 1;
+    }
+    sizes.values().copied().collect()
+}
+
+fn find_root(circuits: &[usize], box_id: usize) -> usize {
+    if circuits[box_id] == box_id {
+        box_id
+    } else {
+        find_root(circuits, circuits[box_id])
+    }
+}
+
+fn all_connected(circuits: &[usize]) -> bool {
+    let first = circuits[0];
+    circuits.iter().all(|v| *v == first)
 }
