d9e0a301d6a9744e08da88b6e3c1733323e08974
Shuflduf <shuflduf@gmail.com>
2025-12-10 18:44:06 -0700

    works but slow

--- src/aoc_10.rs
+++ src/aoc_10.rs
@@ -1,3 +1,7 @@
+use std::collections::HashMap;
+
+// const INPUT: &str =
+// r"[..##..] (0,5) (1,2,3,4,5) (1,3,4,5) (3,4) (2,3,5) (0,1,2,5) {29,40,23,42,39,52}";
 const INPUT: &str = include_str!("../inputs/10.txt");
 // const INPUT: &str = r"[.##.] (3) (1,3) (2) (2,3) (0,2) (0,1) {3,5,4,7}
 // [...#.] (0,2,3,4) (2,3) (0,4) (0,1,2) (1,2,3,4) {7,5,12,7,2}
@@ -7,6 +11,7 @@ const INPUT: &str = include_str!("../inputs/10.txt");
 struct Machine {
     target: Vec<bool>,
     buttons: Vec<Vec<usize>>,
+    joltages: Vec<u32>,
 }
 impl Machine {
     fn new(line: &str) -> Machine {
@@ -24,13 +29,16 @@ impl Machine {
                         .collect()
                 })
                 .collect(),
+            joltages: line[joltage_start + 1..line.len() - 1]
+                .split(",")
+                .map(|joltage| joltage.parse().unwrap())
+                .collect(),
         }
     }
 
-    fn solve(&self) -> u32 {
+    fn solve_part_one(&self) -> u32 {
         let mut min_presses = u32::MAX;
 
-        // i love bitshifting
         for mask in 0..(1 << self.buttons.len()) {
             let mut lights = vec![false; self.target.len()];
 
@@ -50,6 +58,53 @@ impl Machine {
 
         min_presses
     }
+
+    fn solve_part_two(&self) -> u32 {
+        let mut memo = HashMap::new();
+        let res = self.get_min_presses_part_two(vec![0; self.joltages.len()], &mut memo);
+        println!("Solved {:?} with {res} presses", self.joltages);
+        res
+    }
+
+    fn get_min_presses_part_two(&self, state: Vec<u32>, memo: &mut HashMap<Vec<u32>, u32>) -> u32 {
+        if state == self.joltages {
+            return 0;
+        }
+
+        if state.iter().zip(&self.joltages).any(|(s, t)| s > t) {
+            return u32::MAX;
+        }
+
+        if let Some(&result) = memo.get(&state) {
+            return result;
+        }
+
+        if memo.len() % 100000 == 0 && memo.len() != 0 {
+            println!("Explored {} states, current:  {:?}", memo.len(), state);
+        }
+
+        let mut min_presses = u32::MAX;
+
+        for button in &self.buttons {
+            let mut next_state = state.clone();
+
+            for &counter_idx in button {
+                next_state[counter_idx] += 1;
+            }
+
+            let over = next_state.iter().zip(&self.joltages).any(|(s, t)| s > t);
+
+            if !over {
+                let result = self.get_min_presses_part_two(next_state, memo);
+                if result != u32::MAX {
+                    min_presses = min_presses.min(1 + result);
+                }
+            }
+        }
+
+        memo.insert(state, min_presses);
+        min_presses
+    }
 }
 
 fn main() {
@@ -58,7 +113,9 @@ fn main() {
         .filter(|line| !line.is_empty())
         .map(|line| Machine::new(line))
         .collect::<Vec<Machine>>();
-    let part_one_solution: u32 = machines.iter().map(|mach| mach.solve()).sum();
+    let part_one_solution: u32 = machines.iter().map(|mach| mach.solve_part_one()).sum();
+    let part_two_solution: u32 = machines.iter().map(|mach| mach.solve_part_two()).sum();
 
-    println!("Part 1: {part_one_solution}")
+    println!("Part 1: {part_one_solution}");
+    println!("Part 2: {part_two_solution}");
 }
